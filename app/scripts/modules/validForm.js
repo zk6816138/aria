3e1526473ff7091abe75a21e5c4ea86e944d517d
zhoukan <82499295@qq.com>
2020-09-10 20:38:00 +0800

    更改输入框清除按钮

--- app/scripts/modules/validForm.js
+++ app/scripts/modules/validForm.js
@@ -30,10 +30,10 @@ angular.module('validFormModule', [])
 
                 if (attrs.ngClear || attrs.ngClear=='') {
                     var color = attrs.ngClear || '#ddd';
-                    var clearHeight = input.outerHeight()*0.6;
-                    var clearMargin = 3;
+                    var clearHeight = Math.round(input.outerHeight()*0.4);
+                    var clearMargin = 4;
                     var wrap = $compile(angular.element('<div class="valid-form-wrap" style="display: inline-block;position: relative"></div>'))(scope);
-                    var svg = `<?xml version="1.0" encoding="utf-8"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg width="128" height="128" style="width:100%;height:100%;display:block" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" enable-background="new 0 0 1024 1024" xml:space="preserve"><path fill="${color}" d="M512 0 q216.38 6.1 361.65 150.35 q144.25 144.25 150.35 361.65 q-6.1 217.4 -150.35 361.65 q-145.27 144.25 -361.65 150.35 q-217.4 -6.1 -362.67 -150.35 q-143.23 -144.25 -149.33 -361.65 q6.1 -217.4 149.33 -361.65 q145.27 -144.25 362.67 -150.35 ZM512 960 q192 -5.08 317.97 -130.03 q124.95 -125.97 130.03 -317.97 q-5.08 -192 -130.03 -317.97 q-125.97 -124.95 -317.97 -130.03 q-192 5.08 -317.46 130.54 q-125.46 125.46 -130.54 317.46 q5.08 192 130.03 317.97 q125.97 124.95 317.97 130.03 ZM727.37 297.65 q5.07 11.18 4.06 26.41 q-1.02 15.24 -11.18 26.42 l-165.58 165.58 l172.7 165.59 q9.14 10.16 9.14 23.37 q0 12.19 -9.14 22.35 q-5.08 5.07 -10.16 6.09 q-6.1 1.02 -15.24 1.02 q-9.14 0 -15.24 -1.02 q-6.1 -1.02 -11.17 -6.09 l-165.59 -166.61 l-160.51 166.61 q-5.08 5.07 -10.16 6.09 q-5.08 1.02 -15.24 1.02 q-9.14 0 -15.23 -1.02 q-6.1 -1.02 -10.16 -6.09 q-10.16 -10.16 -10.16 -22.35 q0 -13.21 10.16 -23.37 l165.58 -165.59 l-165.58 -172.69 q-10.16 -10.16 -10.16 -22.35 q0 -13.21 10.16 -23.37 q10.16 -8.13 22.35 -8.13 q13.2 0 22.35 8.13 l166.6 173.72 l172.7 -173.72 q10.16 -8.13 22.35 -8.13 q13.2 0 22.35 8.13 Z"/></svg>`;
+                    var svg = `<?xml version="1.0" encoding="utf-8"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg id="remove" width="128" height="128" style="width:100%;height:100%;display: block;" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1792 1792" enable-background="new 0 0 1792 1792" xml:space="preserve"><path fill="${color}" d="M1749.76 1339.47 q42.24 42.24 42.24 102.58 q0 60.34 -42.24 102.57 l-205.14 205.14 q-42.24 42.24 -102.57 42.24 q-60.34 0 -102.58 -42.24 l-443.47 -443.47 l-443.47 443.47 q-42.24 42.24 -102.57 42.24 q-60.34 0 -102.58 -42.24 l-205.14 -205.14 q-42.24 -42.24 -42.24 -102.57 q0 -60.34 42.24 -102.58 l443.47 -443.47 l-443.47 -443.47 q-42.24 -42.24 -42.24 -102.57 q0 -60.34 42.24 -102.58 l205.14 -205.14 q42.24 -42.24 102.58 -42.24 q60.33 0 102.57 42.24 l443.47 443.47 l443.47 -443.47 q42.24 -42.24 102.58 -42.24 q60.34 0 102.57 42.24 l205.14 205.14 q42.24 42.24 42.24 102.58 q0 60.33 -42.24 102.57 l-443.47 443.47 l443.47 443.47 Z"/></svg>`;
                     var clear = $compile(angular.element(`<div class="valid-form-clear" ng-show="modelValue.length > 0" ng-click="clearValue()">${svg}</div>`))(scope);
                     clear.css({
                         position: 'absolute',
@@ -42,7 +42,8 @@ angular.module('validFormModule', [])
                         top: '50%',
                         right: '1px',
                         transform: 'translateY(-50%)',
-                        margin: `0 ${clearMargin}px`,
+                        padding: `${clearMargin}px`,
+                        'box-sizing': 'content-box'
                     });
                     input.wrap(wrap).after(clear).css({'padding-right': `${clearHeight + clearMargin * 2}px`});
 
